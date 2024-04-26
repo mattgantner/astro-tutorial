bec090d48963d4ad07fc090286b8db2f13f115d0
Matt Gantner <matt.gantner@louisvilleky.gov>
2024-04-26 15:02:47 -0400

    added a 3rd party js lib. Added a dark / light theme with vallina js

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,8 @@
 import { defineConfig } from 'astro/config';
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://moonlit-sfogliatella-9e5f27.netlify.app/"
+  site: "https://moonlit-sfogliatella-9e5f27.netlify.app/",
+  integrations: [preact()]
 });
