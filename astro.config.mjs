2a788cbce4747a8c7ec33c246197b3e2179be6c9
Matt Gantner <matt.gantner@louisvilleky.gov>
2024-04-26 14:44:47 -0400

    mostly complete. added rss.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,5 @@
 import { defineConfig } from 'astro/config';
-
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: "https://moonlit-sfogliatella-9e5f27.netlify.app/"
+});
